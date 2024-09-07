5a5934baa85b95d101d1ed185a26fe2641fdf549
Nyarlathotep <anhedonia@skiff.com>
2024-09-06 23:05:14 -0400

    chore(config): @next/bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+import createBundleAnalyzer from '@next/bundle-analyzer'
 import {createVanillaExtractPlugin} from '@vanilla-extract/next-plugin'
 import type {NextConfig} from 'next'
 
 const config: NextConfig = {}
 const withVanillaExtract = createVanillaExtractPlugin()
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-export default withVanillaExtract(config)
+export default withBundleAnalyzer(withVanillaExtract(config))
