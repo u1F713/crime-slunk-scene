69dc274fcada7cf6e35149772abf57f3e4b1e283
Nyarlathotep <anhedonia@skiff.com>
2024-10-04 08:40:14 -0400

    fix(Invalid DOM property): jsx svg syntax

--- app/chronicles/components/image-viewer/image-viewer.tsx
+++ app/chronicles/components/image-viewer/image-viewer.tsx
@@ -64,11 +64,11 @@ const ImageViewer: FunctionComponent<ImgProps> = ({src, alt}) => {
 
         <ul className={styles.toolbar}>
           <li onKeyDown={undefined} onClick={() => downloadImage(src, alt)}>
-            <svg height="1em" width="1em" stroke-linejoin="round">
+            <svg height="1em" width="1em" strokeLinejoin="round">
               <title>donwload</title>
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M8.75 1V1.75V8.68934L10.7197 6.71967L11.25 6.18934L12.3107 7.25L11.7803 7.78033L8.70711 10.8536C8.31658 11.2441 7.68342 11.2441 7.29289 10.8536L4.21967 7.78033L3.68934 7.25L4.75 6.18934L5.28033 6.71967L7.25 8.68934V1.75V1H8.75ZM13.5 9.25V13.5H2.5V9.25V8.5H1V9.25V14C1 14.5523 1.44771 15 2 15H14C14.5523 15 15 14.5523 15 14V9.25V8.5H13.5V9.25Z"
                 fill="currentColor"
               />
