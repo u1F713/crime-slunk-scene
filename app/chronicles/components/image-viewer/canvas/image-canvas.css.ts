4a602b69ea88257521b295a5210d7e7764a952f3
Nyarlathotep <anhedonia@skiff.com>
2024-10-08 23:06:09 -0400

    feat: make image draggable

--- app/chronicles/components/image-viewer/canvas/image-canvas.css.ts
+++ app/chronicles/components/image-viewer/canvas/image-canvas.css.ts
@@ -3,7 +3,4 @@ import {style} from '@vanilla-extract/css'
 export const viewerImage = style({
   position: 'absolute',
   userSelect: 'none',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
 })
--- app/chronicles/components/image-viewer/canvas/image-canvas.tsx
+++ app/chronicles/components/image-viewer/canvas/image-canvas.tsx
@@ -1,4 +1,12 @@
-import type {FunctionComponent} from 'react'
+'use client'
+
+import {
+  type FunctionComponent,
+  type MouseEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import {viewerImage} from './image-canvas.css.ts'
 
 interface CanvasProps {
@@ -7,9 +15,54 @@ interface CanvasProps {
 }
 
 export const ViewerCanvas: FunctionComponent<CanvasProps> = ({src, alt}) => {
+  const [isDragging, setIsDragging] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+  const [offset, setOffset] = useState({x: 0, y: 0})
+  const elementRef = useRef<HTMLImageElement>(null)
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setIsDragging(true)
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    })
+  }
+
+  const handleMouseMove: MouseEventHandler<HTMLImageElement> = e => {
+    if (isDragging)
+      setPosition({
+        x: e.clientX - offset.x,
+        y: e.clientY - offset.y,
+      })
+  }
+
+  useEffect(() => {
+    const rect = elementRef.current?.getBoundingClientRect()
+    if (rect)
+      setPosition({
+        x: window.innerWidth / 2 - rect.width / 2,
+        y: window.innerHeight / 2 - rect.height / 2,
+      })
+  }, [])
+
   return (
     <figure>
-      <img className={viewerImage} draggable="false" src={src} alt={alt} />
+      <img
+        className={viewerImage}
+        draggable="false"
+        ref={elementRef}
+        src={src}
+        alt={alt}
+        onKeyDown={undefined}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={() => setIsDragging(false)}
+        style={{
+          left: `${position.x}px`,
+          top: `${position.y}px`,
+          visibility: elementRef.current ? 'visible' : 'hidden',
+        }}
+      />
     </figure>
   )
 }
