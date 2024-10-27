ecc8a9bad59a61056063c38f45b035ea90e997ed
Nyarlathotep <anhedonia@skiff.com>
2024-10-27 11:58:51 -0400

    config(next): enable experimental.reactCompiler

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import createBundleAnalyzer from '@next/bundle-analyzer'
 import {createVanillaExtractPlugin} from '@vanilla-extract/next-plugin'
 import type {NextConfig} from 'next'
 
-const config: NextConfig = {}
+const config: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+}
 const withVanillaExtract = createVanillaExtractPlugin()
 const withBundleAnalyzer = createBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
