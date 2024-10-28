a8c504f08bb5c5f51d71eb8d7523b9593581d748
Nyarlathotep <anhedonia@skiff.com>
2024-10-27 23:39:50 -0400

    chore(vitest): load env variables

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
+import path from 'node:path'
+import {loadEnvFile} from 'node:process'
 import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import {defaultExclude, defineConfig} from 'vitest/config'
+
+loadEnvFile(path.resolve(import.meta.dirname, '.env.test'))
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin(), tsconfigPaths()],
